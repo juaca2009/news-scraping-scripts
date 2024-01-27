681635b3b2e33e73e930a8bb41a3d51844e9f4a3
juaca2009 <juaca2009@gmail.com>
2024-01-27 12:00:20 -0500

    semana scrap

--- src/semana.py
+++ src/semana.py
@@ -1,26 +1,48 @@
 from load_driver import load_driver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 WAIT_TIME = 30
 NUMER_OF_ITERATIONS = 2
 MORE_NEWS_BUTTON_CLASS = "styles__VerMas-sc-o51gjq-3"
 
-driver = load_driver()
-driver.get('https://www.semana.com/')
+def get_articles(driver):
+    for _ in range(NUMER_OF_ITERATIONS):
+        enlace_mas_contenido = WebDriverWait(driver, WAIT_TIME).until(
+                EC.presence_of_element_located((By.CLASS_NAME, MORE_NEWS_BUTTON_CLASS))
+            )
+        driver.execute_script("arguments[0].click();", enlace_mas_contenido)
+        articles = driver.find_elements(By.CLASS_NAME, "grid-item")
+        driver.implicitly_wait(5)
+    return articles
 
-driver.find_element(By.XPATH, "/html/body/div[1]/div/header/div[7]/"+
-                    "div[4]/div/div[1]/div[3]/nav/a[7]").click()
+def home_button(driver):
+    home_button = driver.find_element(By.XPATH, "/html/body/div[1]/div/header/div[7]/div[4]/div/div[1]/div[1]/div[2]/a") 
+    driver.execute_script("arguments[0].click();", home_button)
 
-for i in range(NUMER_OF_ITERATIONS):
-    enlace_mas_contenido = WebDriverWait(driver, WAIT_TIME).until(
-            EC.presence_of_element_located((By.CLASS_NAME, MORE_NEWS_BUTTON_CLASS))
-        )
-    driver.execute_script("arguments[0].click();", enlace_mas_contenido)
-    articles = driver.find_elements(By.CLASS_NAME, "grid-item")
-    driver.implicitly_wait(5)
+def get_politica(driver):
+    driver.find_element(By.XPATH, "/html/body/div[1]/div/header/div[7]/"+
+                    "div[4]/div/div[1]/div[3]/nav/a[7]").click() # Politica button
+    articles = get_articles(driver)
+    home_button(driver)
 
+def get_deportes(driver):
+    deportes_button = driver.find_element(By.XPATH, "/html/body/div[1]/div/header/div[7]/div[4]/div/div[3]/div/div/div/div[1]/a[6]")
+    driver.execute_script("arguments[0].click();", deportes_button)
+    articles = get_articles(driver)
+    home_button(driver)
 
-print(len(articles))
+def get_tecnologia(driver):
+    tecnologia_button = driver.find_element(By.XPATH, "/html/body/div[1]/div/header/div[7]/div[4]/div/div[3]/div/div/div/div[1]/a[1]")
+    driver.execute_script("arguments[0].click();", tecnologia_button)
+    articles = get_articles(driver)
+    home_button(driver)
 
+if __name__ == "__main__":
+    driver = load_driver()
+    driver.get('https://www.semana.com/')
+    #get_politica(driver)
+    #get_deportes(driver)
+    get_tecnologia(driver)
